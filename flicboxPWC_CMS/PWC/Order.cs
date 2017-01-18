30a26d344e65ac061324eb0e834b57416ae2f872
Writer1234 <nik.bakalkar@gmail.com>
2017-01-19 01:14:23 +0530

    order class with Gift Properties in precheckout class

--- flicboxPWC_CMS/PWC/Order.cs
+++ flicboxPWC_CMS/PWC/Order.cs
@@ -30,22 +30,44 @@ namespace flicboxPWC_CMS.PWC
 
         }
 
+        public bool GetGiftItemExists()
+        {
+            bool IsGift = false;
+            var items = ShoppingCart.Instance.Items;
+            var productlist = new List<ProductMaster>();
+
+            foreach (CartItem item in items)
+            {
+                ProductMaster obj = new ProductMaster(item.ProductId);
+                if(obj.SubscriptionName.ToUpper()=="GIFT")
+                {
+                    IsGift = true;
+                    break;
+                }
+  
+            }
+            return IsGift;
+        }
+
 
         private static DataTable CreateDataTable(List<CartItem> items)
         {
             DataTable table = new DataTable();
+            //int i = 1;
+            //table.Columns.Add("RecID", typeof(int));
             table.Columns.Add("ProductId", typeof(long));
             table.Columns.Add("Quantity", typeof(int));
             table.Columns.Add("UnitPrice", typeof(float));
             foreach (CartItem id in items)
             {
                 table.Rows.Add(id.ProductId, id.Quantity, id.UnitPrice);
+                //i++;
             }
             return table;
         }
 
 
-        public string AddOrder(OrderMater obj, List<CartItem> CartItems, out Int64 OrderID, out bool IsError)
+        public string AddOrder(OrderMater obj, List<CartItem> CartItems, out Int64 OrderID, out bool IsError,out  int CustIDLoginID)
         {
             string message = string.Empty;
 
@@ -53,8 +75,10 @@ namespace flicboxPWC_CMS.PWC
             {
                 using (SqlConnection Con = new SqlConnection(conStr))
                 {
-
+                    
                     SqlCommand cmd = new SqlCommand();
+
+                    cmd.Connection = Con;
                     cmd.CommandText = "[dbo].[OrderMasterAdd]";
                     cmd.CommandType = System.Data.CommandType.StoredProcedure;
 
@@ -86,8 +110,8 @@ namespace flicboxPWC_CMS.PWC
                     cmd.Parameters.Add("@SP_PASSWORD", SqlDbType.VarChar, 50);
                     cmd.Parameters["@SP_PASSWORD"].Value = obj.password;
 
-                    cmd.Parameters.Add("@billingAddress", SqlDbType.NVarChar, 500);
-                    cmd.Parameters["@billingAddress"].Value = obj.Address;
+                    cmd.Parameters.Add("@SP_BillAddrss", SqlDbType.NVarChar, 500);
+                    cmd.Parameters["@SP_BillAddrss"].Value = obj.Address;
 
                     cmd.Parameters.Add("@SP_City", SqlDbType.VarChar, 100);
                     cmd.Parameters["@SP_City"].Value = obj.City;
@@ -116,6 +140,65 @@ namespace flicboxPWC_CMS.PWC
                     cmd.Parameters.Add("@SP_TotalAmount", SqlDbType.Float, 0);
                     cmd.Parameters["@SP_TotalAmount"].Value = obj.TotalAmount;
 
+                    if(obj.Precheckout.IsGiftExist==true)
+                    {
+                        cmd.Parameters.Add("@SP_ISGIFT", SqlDbType.Bit, 0);
+                        cmd.Parameters["@SP_ISGIFT"].Value = obj.Precheckout.IsGiftExist;
+
+                        cmd.Parameters.Add("@SP_SenderName", SqlDbType.VarChar, 150);
+                        cmd.Parameters["@SP_SenderName"].Value = obj.Precheckout.GiftSenderName;
+
+                        cmd.Parameters.Add("@SP_SenderEmail", SqlDbType.VarChar, 100);
+                        cmd.Parameters["@SP_SenderEmail"].Value = obj.Precheckout.GiftSenderEmailID;
+
+                        cmd.Parameters.Add("@SP_SenderPhone", SqlDbType.VarChar, 20);
+                        cmd.Parameters["@SP_SenderPhone"].Value = obj.Precheckout.GiftSenderPhone;
+
+                        cmd.Parameters.Add("@SP_SenderAddress", SqlDbType.NVarChar, 500);
+                        cmd.Parameters["@SP_SenderAddress"].Value = obj.Precheckout.GiftSenderAddress;
+
+                        cmd.Parameters.Add("@SP_SenderCity", SqlDbType.VarChar, 100);
+                        cmd.Parameters["@SP_SenderCity"].Value = obj.Precheckout.GiftSenderCity;
+
+                        cmd.Parameters.Add("@SP_SenderState", SqlDbType.VarChar, 100);
+                        cmd.Parameters["@SP_SenderState"].Value = obj.Precheckout.GiftSenderState;
+
+                        cmd.Parameters.Add("@SP_SenderPincode", SqlDbType.VarChar, 20);
+                        cmd.Parameters["@SP_SenderPincode"].Value = obj.Precheckout.GiftSenderPincode;
+
+                        cmd.Parameters.Add("@SP_RecieverName", SqlDbType.VarChar, 150);
+                        cmd.Parameters["@SP_RecieverName"].Value = obj.Precheckout.GiftRecieverName;
+
+                        cmd.Parameters.Add("@SP_RecieverEmail", SqlDbType.VarChar, 100);
+                        cmd.Parameters["@SP_RecieverEmail"].Value = obj.Precheckout.GiftRecieverEmailID;
+
+                        cmd.Parameters.Add("@SP_RecieverPhone", SqlDbType.VarChar, 20);
+                        cmd.Parameters["@SP_RecieverPhone"].Value = obj.Precheckout.GiftRecieverPhone;
+
+                        cmd.Parameters.Add("@SP_RecieverAddress", SqlDbType.NVarChar, 500);
+                        cmd.Parameters["@SP_RecieverAddress"].Value = obj.Precheckout.GiftRecieverAddress;
+
+                        cmd.Parameters.Add("@SP_RecieverCity", SqlDbType.VarChar, 100);
+                        cmd.Parameters["@SP_RecieverCity"].Value = obj.Precheckout.GiftRecieverCity;
+
+                        cmd.Parameters.Add("@SP_RecieverState", SqlDbType.VarChar, 100);
+                        cmd.Parameters["@SP_RecieverState"].Value = obj.Precheckout.GiftRecieverState;
+
+                        cmd.Parameters.Add("@SP_RecieverPincode", SqlDbType.VarChar, 100);
+                        cmd.Parameters["@SP_RecieverPincode"].Value = obj.Precheckout.GiftRecieverPincode;
+
+                        cmd.Parameters.Add("@SP_OccasionDetail", SqlDbType.NVarChar, -1);
+                        cmd.Parameters["@SP_OccasionDetail"].Value = obj.Precheckout.OccasionDetails;
+
+                        cmd.Parameters.Add("@SP_PersonalGiftMsg", SqlDbType.NVarChar, -1);
+                        cmd.Parameters["@SP_PersonalGiftMsg"].Value = obj.Precheckout.PersonalGiftMsg;
+
+                        cmd.Parameters.Add("@SP_OccasionDate", SqlDbType.NVarChar,50);
+                        cmd.Parameters["@SP_OccasionDate"].Value = obj.Precheckout.OccasionDate;
+
+                    } 
+
+
                     cmd.Parameters.Add("@RETURNSTATUS", SqlDbType.SmallInt, 0);
                     cmd.Parameters["@RETURNSTATUS"].Direction = ParameterDirection.ReturnValue;
 
@@ -131,11 +214,12 @@ namespace flicboxPWC_CMS.PWC
                     {
                         cmd.ExecuteNonQuery();
 
-                        string status = (string)cmd.Parameters["@RETURNSTATUS"].Value;
-                        string msg = (string)cmd.Parameters["@SP_MESSAGE"].Value;
-                        if (status == "0")
+                        int status = Convert.ToInt32(cmd.Parameters["@RETURNSTATUS"].Value);
+                        string msg = Convert.ToString(cmd.Parameters["@SP_MESSAGE"].Value);
+                        if (status == 0)
                         {
-                            OrderID = (Int64)cmd.Parameters["@RETURNSTATUS"].Value;
+                            CustIDLoginID= Convert.ToInt32(cmd.Parameters["@SP_USERID"].Value);
+                            OrderID =  Convert.ToInt32(cmd.Parameters["@SP_ORDERID"].Value);
                             message = msg;
                             IsError = false;
                             return message;
@@ -144,6 +228,7 @@ namespace flicboxPWC_CMS.PWC
                     }
                     catch (Exception ex)
                     {
+                        CustIDLoginID = 0;
                         OrderID = 0;
                         IsError = true;
                         message = ex.ToString();
@@ -157,11 +242,13 @@ namespace flicboxPWC_CMS.PWC
             catch (System.Threading.ThreadInterruptedException) { }
             catch (Exception ex)
             {
+                CustIDLoginID = 0;
                 OrderID = 0;
                 IsError = true;
                 message = ex.Message;
                 // throw;
             }
+            CustIDLoginID = 0;
             OrderID = 0;
             IsError = true;
             return message;
@@ -181,10 +268,6 @@ namespace flicboxPWC_CMS.PWC
         public string Phone { get; set; }
         public string Email { get; set; }
         public string password { get; set; }
-        public bool IsGift { get; set; }
-        public string ToGiftName { get; set; }
-        public string GiftAddress { get; set; }
-        public string GiftMessage { get; set; }
 
         public int TotalQty { get; set; }
         public float TotalAmount { get; set; }
@@ -217,6 +300,41 @@ namespace flicboxPWC_CMS.PWC
 
         public string preferencesDiet { get; set; }
         public string birthdate { get; set; }
+
+        public bool IsGiftExist { get; set; }
+
+        public string GiftSenderName { get; set; }
+
+        public string GiftSenderEmailID { get; set; }
+
+        public string GiftSenderPhone { get; set; }
+        public string GiftSenderAddress { get; set; }
+
+        public string GiftSenderCity { get; set; }
+
+        public string GiftSenderState { get; set; }
+
+        public string GiftSenderPincode { get; set; }
+
+        public string GiftRecieverName { get; set; }
+
+        public string GiftRecieverEmailID { get; set; }
+
+        public string GiftRecieverPhone { get; set; }
+
+        public string GiftRecieverAddress { get; set; }
+
+        public string GiftRecieverCity { get; set; }
+
+        public string GiftRecieverState { get; set; }
+
+        public string GiftRecieverPincode { get; set; }
+
+        public string OccasionDetails { get; set; }
+
+        public string PersonalGiftMsg { get; set; }
+        public string OccasionDate { get; set; }
+        
     }
 
     public class OrderDetails
